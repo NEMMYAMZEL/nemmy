ed8e71733712b643c076cf7530f653080c6fb0a7
NEMMYAMZEL <140238555+NEMMYAMZEL@users.noreply.github.com>
2023-07-22 12:38:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ const fetchData = () => {
         }
 
         // Check if the iteration is over
-        // Run amimation if so
-        if ( dataArr.length === dataArr.indexOf(customData) + 1 ) {
+        // Run animation if so
+        if (dataArr.length === dataArr.indexOf(customData) + 1) {
           animationTimeline();
         }
       });
@@ -26,52 +26,52 @@ const fetchData = () => {
 
 // Animation Timeline
 const animationTimeline = () => {
-  // Spit chars that needs to be animated individually
+  // Spit chars that need to be animated individually
   const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
   const hbd = document.getElementsByClassName("wish-hbd")[0];
 
   textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
     .split("")
-    .join("</span><span>")}</span`;
+    .join("</span><span>")}</span>`;
 
   hbd.innerHTML = `<span>${hbd.innerHTML
     .split("")
-    .join("</span><span>")}</span`;
+    .join("</span><span>")}</span>`;
 
   const ideaTextTrans = {
     opacity: 0,
     y: -20,
     rotationX: 5,
-    skewX: "15deg"
+    skewX: "15deg",
   };
 
   const ideaTextTransLeave = {
     opacity: 0,
     y: 20,
     rotationY: 5,
-    skewX: "-15deg"
+    skewX: "-15deg",
   };
 
   const tl = new TimelineMax();
 
   tl
     .to(".container", 0.1, {
-      visibility: "visible"
+      visibility: "visible",
     })
     .from(".one", 0.7, {
       opacity: 0,
-      y: 10
+      y: 10,
     })
     .from(".two", 0.4, {
       opacity: 0,
-      y: 10
+      y: 10,
     })
     .to(
       ".one",
       0.7,
       {
         opacity: 0,
-        y: 10
+        y: 10,
       },
       "+=2.5"
     )
@@ -80,13 +80,13 @@ const animationTimeline = () => {
       0.7,
       {
         opacity: 0,
-        y: 10
+        y: 10,
       },
       "-=1"
     )
     .from(".three", 0.7, {
       opacity: 0,
-      y: 10
+      y: 10,
       // scale: 0.7
     })
     .to(
@@ -94,28 +94,28 @@ const animationTimeline = () => {
       0.7,
       {
         opacity: 0,
-        y: 10
+        y: 10,
       },
       "+=2"
     )
     .from(".four", 0.7, {
       scale: 0.2,
-      opacity: 0
+      opacity: 0,
     })
     .from(".fake-btn", 0.3, {
       scale: 0.2,
-      opacity: 0
+      opacity: 0,
     })
     .staggerTo(
       ".hbd-chatbox span",
       0.5,
       {
-        visibility: "visible"
+        visibility: "visible",
       },
       0.05
     )
     .to(".fake-btn", 0.1, {
-      backgroundColor: "rgb(127, 206, 248)"
+      backgroundColor: "rgb(127, 206, 248)",
     })
     .to(
       ".four",
@@ -123,7 +123,7 @@ const animationTimeline = () => {
       {
         scale: 0.2,
         opacity: 0,
-        y: -150
+        y: -150,
       },
       "+=0.7"
     )
@@ -136,7 +136,7 @@ const animationTimeline = () => {
       scale: 1.2,
       x: 10,
       backgroundColor: "rgb(21, 161, 237)",
-      color: "#fff"
+      color: "#fff",
     })
     .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-4", 0.7, ideaTextTrans)
@@ -150,7 +150,7 @@ const animationTimeline = () => {
         skewY: "-5deg",
         y: 50,
         z: 10,
-        opacity: 0
+        opacity: 0,
       },
       "+=0.5"
     )
@@ -159,7 +159,7 @@ const animationTimeline = () => {
       0.7,
       {
         rotation: 90,
-        x: 8
+        x: 8,
       },
       "+=0.4"
     )
@@ -168,7 +168,7 @@ const animationTimeline = () => {
       0.7,
       {
         scale: 0.2,
-        opacity: 0
+        opacity: 0,
       },
       "+=2"
     )
@@ -179,7 +179,7 @@ const animationTimeline = () => {
         scale: 3,
         opacity: 0,
         rotation: 15,
-        ease: Expo.easeOut
+        ease: Expo.easeOut,
       },
       0.2
     )
@@ -190,21 +190,21 @@ const animationTimeline = () => {
         scale: 3,
         opacity: 0,
         rotation: -15,
-        ease: Expo.easeOut
+        ease: Expo.easeOut,
       },
       0.2,
       "+=1"
     )
     .staggerFromTo(
       ".baloons img",
-      2.5,
+      5, // Increase the duration to slow down the balloons effect
       {
         opacity: 0.9,
-        y: 1400
+        y: 1400,
       },
       {
         opacity: 1,
-        y: -1000
+        y: -1000,
       },
       0.2
     )
@@ -216,15 +216,15 @@ const animationTimeline = () => {
         opacity: 0,
         x: 25,
         y: -25,
-        rotationZ: -45
+        rotationZ: -45,
       },
       "-=2"
     )
     .from(".hat", 0.5, {
-      x: -100,
-      y: 350,
+      x: -50, // Adjust the x position
+      y: 300, // Adjust the y position
       rotation: -180,
-      opacity: 0
+      opacity: 0,
     })
     .staggerFrom(
       ".wish-hbd span",
@@ -235,7 +235,7 @@ const animationTimeline = () => {
         // scale: 0.3,
         rotation: 150,
         skewX: "30deg",
-        ease: Elastic.easeOut.config(1, 0.5)
+        ease: Elastic.easeOut.config(1, 0.5),
       },
       0.1
     )
@@ -244,13 +244,13 @@ const animationTimeline = () => {
       0.7,
       {
         scale: 1.4,
-        rotationY: 150
+        rotationY: 150,
       },
       {
         scale: 1,
         rotationY: 0,
         color: "#ff69b4",
-        ease: Expo.easeOut
+        ease: Expo.easeOut,
       },
       0.1,
       "party"
@@ -261,7 +261,7 @@ const animationTimeline = () => {
       {
         opacity: 0,
         y: 10,
-        skewX: "-15deg"
+        skewX: "-15deg",
       },
       "party"
     )
@@ -273,21 +273,21 @@ const animationTimeline = () => {
         opacity: 0,
         scale: 80,
         repeat: 3,
-        repeatDelay: 1.4
+        repeatDelay: 1.4,
       },
       0.3
     )
     .to(".six", 0.5, {
       opacity: 0,
       y: 30,
-      zIndex: "-1"
+      zIndex: "-1",
     })
     .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
     .to(
       ".last-smile",
       0.5,
       {
-        rotation: 90
+        rotation: 90,
       },
       "+=1"
     );
